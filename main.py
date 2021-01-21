1839a26d73360043fa2d1c10bebfd43e61415a50
PythonEESD2021 <ctran@eesd.org>
2021-01-21 23:32:05 +0000

    typecasting and input

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 print("Hello" + " World.")
 print("Hello" + " " + "World." + " Today is Thursday.")
 # This is a concatenation of strings
+print()
+print("Tomorrow is Friday.")
+
+# If adding strings is called "concatenation"
+# What is + for integers called? addition
+# 5 + 4 = 9
+
+print("5" + "4") 
+print(5 + 4)
+
+firstName = "Lalyn" #string
+print("My name is " + firstName + ".")
+age = 21 #int
+print("My age is " + str(age))
+
+# firstName != FirstName !(not)=(equal)
+
+userName = input("What is your name? ")
+print("Hello " + userName)
+
+# Ask the user for their birth year
+userBirth = int(input("What is your birth year? "))
+print(2021-userBirth)
